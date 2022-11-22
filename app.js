7621d0fded67be4e1d6bf0742156bfde645d9e4e
Jayadeepsai <jayadeepsaim@gmail.com>
2022-11-22 15:33:51 +0530

    Routes

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const app = express();
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
+const productRoutes =require('./Routes/productRoute')
 
-mongoose.connect('mongodb+srv://Deepu:7700@cluster0.5yjfycv.mongodb.net/?retryWrites=true&w=majority')
+
+mongoose.connect('mongodb+srv://Deepu:7700@cluster0.5yjfycv.mongodb.net/POS?retryWrites=true&w=majority')
 
 
 app.use(bodyParser.urlencoded({extended: false})); // true allows it to parse rich data , false allows you to parse simple data
 app.use(bodyParser.json());
+
+app.use('/ProductRoutes',productRoutes)
 
 
 
--- server.js
+++ server.js
@@ -7,4 +7,4 @@ const server =http.createServer(app)
 
 server.listen(port)
 
-console.log("server is running on port"+port)
+console.log("server is running on port" + port)
